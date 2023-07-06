174fa80a99e798507d5cfe8025902a63e6860e99
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-07-06 12:25:00 +0530

    Code changed to add to wo number s

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+   public static void Main()
+    {
+        Console.WriteLine("Enter the first number: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2;
+
+        Console.WriteLine("The sum of {0} and {1} is {2}", number1, number2, sum);
+
+        Console.ReadLine();
+    }
+}
